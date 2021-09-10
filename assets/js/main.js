c60bdfa63240b688c0f88d3944344aea7d970700
white-marshmallow <89884207+white-marshmallow@users.noreply.github.com>
2021-09-10 11:27:52 +0900

    Create main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,12 +31,6 @@ const app = Vue.createApp({
           // q3: 'ききき',
         }
       },
-      stage4: {
-          q1: 'おおお',
-          // q2: 'かかか',
-          // q3: 'ききき',
-        }
-      },
 
       /* それぞれの問題が正解かどうか
       *  ex. 問題2-3を追加する場合は配列にfalseを追加します。
@@ -56,19 +50,12 @@ const app = Vue.createApp({
           // false, // 3-3
         ]
       },
-        stage4: [
-          false, // 3-1
-          // false, // 3-2
-          // false, // 3-3
-        ]
-      },
 
       /* ステージの問題が全て正解かどうか */
       clear: {
         stage1: false,
         stage2: false,
         stage3: false,
-        stage4: false,
       },
 
       /* 次のステージを表示するかどうか
@@ -77,7 +64,6 @@ const app = Vue.createApp({
       next: {
         stage1: false,
         stage2: false,
-        stage3: false,
       },
     }
   },
